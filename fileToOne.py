40090459a472775de0e9454bfb61d49dd68807be
taoyy <tyyphp@163.com>
2021-11-19 08:29:54 +0800

    add fileToOne program

---
+++ fileToOne.py
@@ -0,0 +1,18 @@
+import win32com.client as win32
+import os
+word = win32.gencache.EnsureDispatch('Word.Application')
+#启动word对象应用
+word.Visible = False
+path = r'D:\\WWW\\GitBase\\ArticleCheck\\file'
+files = []
+for filename in os.listdir(path):
+    filename = os.path.join(path,filename)
+    files.append(filename)
+#新建合并后的文档
+output = word.Documents.Add()
+for file in files:
+    output.Application.Selection.InsertFile(file)#拼接文档
+#获取合并后文档的内容
+doc = output.Range(output.Content.Start, output.Content.End)
+output.SaveAs('D:\\result.docx') #保存
+output.Close()
